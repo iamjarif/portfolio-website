46f45504de321d3329df91b87447b936b488dfb6
Kazi Suny Al Jarif <jarif@iut-dhaka.edu>
2023-11-03 14:22:52 +0600

    added frontend

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can change the port number if needed
+const port = 3001; // You can change the port number if needed
 
-// Define a route for the homepage
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// Define your portfolio data
+const portfolioData = {
+  name: 'Your Name',
+  description: 'Web Developer | Designer | Your Description Here',
+};
+
+// Define routes
 app.get('/', (req, res) => {
-  res.send('Hello, I am Jarif');
+  res.render('index', portfolioData);
+});
+
+app.get('/cv', (req, res) => {
+  res.send('CV Page');
+});
+
+app.get('/projects', (req, res) => {
+  res.send('Projects Page');
 });
 
 // Start the server
